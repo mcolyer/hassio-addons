87e9e9d78188f942013974f2ed53dd9119b281ba
Matt Colyer <matt@colyer.name>
2017-11-19 07:15:27 -0800

    Use proper string concatenation

--- honeywell-sensors/honeywell-sensors.sh
+++ honeywell-sensors/honeywell-sensors.sh
@@ -24,5 +24,5 @@ do
   # Create file with touch /tmp/honeywell.log if logging is needed
   [ -w /tmp/honeywell.log ] && echo $line >> honeywell.log
   serial="$(echo $line | jq --raw-output '.serial')"
-  echo $line | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i honeywell-sensors -r -l -t $MQTT_TOPIC+"/"+$serial
+  echo $line | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i honeywell-sensors -r -l -t "$MQTT_TOPIC/$serial"
 done
