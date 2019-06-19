6f5057e846b185bb49925888f65430fbc3664292
Matt Colyer <matt@colyer.name>
2019-06-19 15:46:15 -0700

    Fix the config parsing

--- mb8600/run.sh
+++ mb8600/run.sh
@@ -4,8 +4,8 @@ export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
 CONFIG_PATH=/data/options.json
-INFLUXDB_ADDRESS=$(jq --raw-output '.influxdb_address' $CONFIG_PATH)
-INFLUXDB_DATABASE=$(jq --raw-output '.influxdb_database' $CONFIG_PATH)
+INFLUXDB_ADDRESS=$(jq --raw-output '.influxdb.address' $CONFIG_PATH)
+INFLUXDB_DATABASE=$(jq --raw-output '.influxdb.database' $CONFIG_PATH)
 
 cd /
 ./mb8600 -influxdb-address $INFLUXDB_ADDRESS -influxdb-database $INFLUXDB_DATABASE
