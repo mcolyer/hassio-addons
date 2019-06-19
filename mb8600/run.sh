bd50a404327ef2ba521c0269018e8258db808fbf
Matt Colyer <matt@colyer.name>
2019-06-19 15:36:52 -0700

    Switching to a different project

--- mb8600/run.sh
+++ mb8600/run.sh
@@ -4,9 +4,9 @@ export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
 CONFIG_PATH=/data/options.json
+INFLUXDB_ADDRESS=$(jq --raw-output '.influxdb_address' $CONFIG_PATH)
+INFLUXDB_DATABASE=$(jq --raw-output '.influxdb_database' $CONFIG_PATH)
 
-#set -x  ## uncomment for MQTT logging...
+cd /
+./mb8600 -influxdb-address $INFLUXDB_ADDRESS -influxdb-database $INFLUXDB_DATABASE
 
-cd /modem-statistics/models/mb8600/
-cp $CONFIG_PATH config.json
-node server.js
