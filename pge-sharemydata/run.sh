f184a8b7837cebed00c495b2a1bb7bd567381cf0
Matt Colyer <matt@colyer.name>
2019-12-13 10:13:13 -0800

    Update export

--- pge-sharemydata/run.sh
+++ pge-sharemydata/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bashio
 
 
-export INFLUXDB_URL=$(bashio::config 'influxdb_url')
-export INFLUXDB_DB=$(bashio::config 'influxdb_db')
-export PGE_BULK_ID=$(bashio::config 'pge_bulk_id')
-export PGE_CLIENT_ID=$(bashio::config 'pge_client_id')
-export PGE_CLIENT_SECRET=$(bashio::config 'pge_client_secret')
-export SSL_CERT=$(bashio::config 'ssl_cert')
-export SSL_KEY=$(bashio::config 'ssl_key')
+export INFLUXDB_URL="$(bashio::config 'influxdb_url')"
+export INFLUXDB_DB="$(bashio::config 'influxdb_db')"
+export PGE_BULK_ID="$(bashio::config 'pge_bulk_id')"
+export PGE_CLIENT_ID="$(bashio::config 'pge_client_id')"
+export PGE_CLIENT_SECRET="$(bashio::config 'pge_client_secret')"
+export SSL_CERT="$(bashio::config 'ssl_cert')"
+export SSL_KEY="$(bashio::config 'ssl_key')"
 
 bashio::log.info "Starting server"
 
