161e8bf208b5086cd23031d87da934627d8dcd03
Matt Colyer <matt@colyer.name>
2020-04-23 14:31:22 -0700

    Update tesla-solar-fetcher to make it more resilent

--- tesla-solar-fetcher/run.sh
+++ tesla-solar-fetcher/run.sh
@@ -5,6 +5,9 @@ export PASSWORD=$(bashio::config 'password')
 INFLUXDB_URL=$(bashio::config 'influxdb_url')
 INFLUXDB_DB=$(bashio::config 'influxdb_db')
 
+# Keep it running if a specific run happens to fail
+set +o errexit
+
 bashio::log.info "Starting event listener..."
 while read -r input; do
     bashio::log.info "Fetching"
